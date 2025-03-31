e3782ab1473aa8fe64296a6d25af7291f4e344a8
Aleksandr Omelucik <bladewizard@mail.ru>
2025-03-31 08:42:39 +0300

    update

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-let button = document.querySelector('button');
-let counter = 0;
+let button = document.getElementById('button+');
+let counterText = document.getElementById('counter')
+let buttonReset = document.getElementById('button-')
+const COUNTER_INITIAL_VALUE = 0;
+let counter = COUNTER_INITIAL_VALUE;
 button.addEventListener('click', function(){
-    counter = counter + 1;
+    counter = counter + parseInt(button.dataset.pools);
+    counterText.innerText = counter;
 });
+buttonReset.addEventListener('click', function () {
+    counter = parseInt(buttonReset.dataset.pools);
+    counterText.innerText = counter;
+})
+console.log(counterText.dataset)
